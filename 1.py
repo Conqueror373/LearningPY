1526ddf62baa569af5d42a3725b033885fb35d74
Conqueror373 <66438649+Conqueror373@users.noreply.github.com>
2023-02-05 02:30:59 +0300

    Add files via upload

---
+++ 1.py
@@ -0,0 +1,63 @@
+global data, columns
+FileN = input(str("Путь до файла .csv: ")) #"C:\File\data.csv"
+
+
+
+def get_line():
+    global s
+    s = list(input("Айди, Имя пользователя, Логин, Пароль, Статус аккаунта и цену в долларах: ").split())
+    return (s[0], s[1], s[2], s[3], s[4], int(s[5]))
+
+def insert(line):
+    if line not in data:
+        data.append(line)
+        write_to_file(FileN)
+
+
+def write_to_file(FileN):
+    with open(FileN, 'a') as file:
+        if "Nickname" in open(FileN).read():  
+            for line in data:
+                line=[str(i) for i in line]
+                file.write(','.join(line)+'\n')
+        else:
+            file.write(','.join(columns)+'\n')
+            for line in data:
+                line=[str(i) for i in line]
+                file.write(','.join(line)+'\n')
+
+def read_from_file(FileN):
+    with open(FileN, 'r') as file:
+        columns = tuple(file.readline().split(','))
+        data = []
+        for line in file:
+            line = line.split(',')
+            data.append((line[0], line[1], line[2], line[3], line[4], int(line[5])))
+    for i in columns:
+        print(str(i).ljust(size), end=" ")    
+
+    for i in data:
+        print(str(i).ljust(size), end=" ")
+    print()
+
+    for line in data:
+        for i in line:
+            print(str(i).ljust(size), end=' ')
+        print()
+
+
+
+
+
+
+
+size = 11
+columns = ("Id", "Nickname", "Login", "Password", "Status", "Price in $")
+data = []
+
+
+
+
+#write_to_file(FileN)
+insert(get_line())
+print(read_from_file(FileN))
